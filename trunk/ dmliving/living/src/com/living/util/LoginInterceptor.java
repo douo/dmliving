8d640bbd2dbbec8718b7006e7c3b171028b172e9
cdlmagical <cdlmagical@15b6cb02-df24-11de-bf67-198b766644d4>
2009-12-12 18:54:04 +0000

    修改转向登陆前页面功能, 增强到是否登陆判断!

--- trunk/ dmliving/living/src/com/living/util/LoginInterceptor.java
+++ trunk/ dmliving/living/src/com/living/util/LoginInterceptor.java
@@ -3,11 +3,12 @@ package com.living.util;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.StrutsStatics;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.MethodFilterInterceptor;
 
@@ -21,29 +22,63 @@ public class LoginInterceptor extends MethodFilterInterceptor {
 	
 	private static final Logger log = Logger.getLogger(LoginInterceptor.class);
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected String doIntercept(ActionInvocation invocation) throws Exception {
-		HttpServletRequest request = ServletActionContext.getRequest();
-		HttpSession session = request.getSession();
-		// 判断用户是否登陆
-		if (session != null && session.getAttribute(Constants.SESSION_LOGIN) != null) {
+		/*HttpServletRequest request = ServletActionContext.getRequest();
+		HttpSession session = request.getSession();*/
+		ActionContext actionContext = invocation.getInvocationContext();
+		HttpServletRequest request = (HttpServletRequest) actionContext.get(StrutsStatics.HTTP_REQUEST);
+		Map session = actionContext.getSession();
+		
+		// 如果登陆则不做拦截处理
+		if (session != null && session.get(Constants.SESSION_LOGIN) != null) {
 			return invocation.invoke();
 		}
+		
+		/*Cookie[] cookies = request.getCookies();    
+        if (cookies!=null) {    
+            for (Cookie cookie : cookies) {    
+                if (COOKIE_REMEMBERME_KEY.equals(cookie.getName())) {    
+                    String value = cookie.getValue();    
+                    if (StringUtils.isNotBlank(value)) {    
+                        String[] split = value.split("==");    
+                        String userName = split[0];    
+                        String password = split[1];    
+                        try {    
+                            User user = userDao    
+                                    .attemptLogin(userName, password);    
+                            session.put(USER_SESSION_KEY, user);    
+                        } catch (UserNotFoundException e) {    
+                            setGoingToURL(session, invocation);    
+                            return "login";    
+                        }    
+                    } else {    
+                        setGoingToURL(session, invocation);    
+                        return "login";    
+                    }    
+                    return invocation.invoke();    
+                }    
+            }    
+        }*/
+		
 		// 设置原请求地址
+		// setGoingToURL(request, session, invocation);
+		log.info("Going to login...");
 		setGoingToURL(request, session, invocation);
-		return invocation.invoke();
+		return "login";
 	}
 	
 	/**
 	 * 设置登陆前页面URL
-	 * @param request
 	 * @param session
 	 * @param invocation
 	 */
 	@SuppressWarnings("unchecked")
-	private void setGoingToURL(HttpServletRequest request,HttpSession session,ActionInvocation invocation) {
+	//private void setGoingToURL(HttpServletRequest request,HttpSession session,ActionInvocation invocation) {
+	private void setGoingToURL(HttpServletRequest request, Map session, ActionInvocation invocation) {
 		// 如果referer不为空 直接使用它。如果为空我们分别获得命名空间，action名,以及请求参数重新构造成一个URL保存在session中
-		String url = request.getHeader("referer");
+		/*String url = request.getHeader("referer");
 		if (url == null || "".equals(url)) {
 			url = "";
 			String path = request.getContextPath();
@@ -70,7 +105,19 @@ public class LoginInterceptor extends MethodFilterInterceptor {
 		// TODO 在这里写例外URL
 		
 		log.info("待转向URL: " + url);
-		session.setAttribute(Constants.GOTO_URL_KEY, url);
+		session.setAttribute(Constants.GOTO_URL_KEY, url);*/
+		
+		String url = "";
+		String nameSpace = invocation.getProxy().getNamespace();
+		String actionName = invocation.getProxy().getActionName();
+		if (StringUtils.isNotBlank(nameSpace) && !nameSpace.equals("/")) {
+			url += nameSpace;
+		}
+		if (StringUtils.isNotBlank(actionName)) {
+			url += "/" + actionName + ".action";
+		}
+		log.info("待转向URL: " + url);
+		session.put(Constants.GOTO_URL_KEY, url);
 	}
 
 }
